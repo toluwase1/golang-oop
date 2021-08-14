98318dc13facedb7b7a51b548d630dec07e71863
toluwase <toluwasethomas1@gmail.com>
2021-08-14 12:08:36 +0100

    oop methods

---
+++ interfaces.go
@@ -0,0 +1,38 @@
+package main
+
+/*
+Interfaces allows us to do polymorphism
+Interfaces allows us to define behaviour
+A value can be of more than one type
+If you have this method , you are my type, thats interface
+Staffs
+
+· Principal| Teachers | Non-Academic Staffs | Students | Courses | Classes | Applicant
+	- A teacher can grade a student in a course he or she teaches.
+	- A student can take a course.
+	- The principal can expel a student etc.
+	- A principal can admit applicants based on age.
+ */
+
+func teachCourse()  {
+
+}
+
+func gradeStudents()  {
+
+}
+func takeCourse()  {
+
+}
+
+func expelStudent()  {
+
+}
+
+func admitApplicants()  {
+
+}
+
+func main() {
+
+}
--- main.go
+++ main.go
@@ -1,1 +1,21 @@
-package golang_OOP
+package main
+
+type BankAccount struct {
+	Owner string
+	balance float64
+}
+
+func (ba BankAccount) Balance() float64 {
+	return ba.balance
+}
+
+func (ba *BankAccount) Deposit(amount float64)  {
+	ba.balance+=amount
+}
+func (ba *BankAccount) Withdraw()  {
+
+}
+
+func main() {
+	
+}
